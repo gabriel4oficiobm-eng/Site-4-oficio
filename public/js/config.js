a86838da4025bd494ff2ae3a0a6b4d804f775aa2
gabriel4oficiobm-eng <gabriel4oficiobm@gmail.com>
2026-03-02 21:36:18 -0300

    Create config.js for Supabase environment variables
    Added configuration file for environment variables.

---
+++ public/js/config.js
@@ -0,0 +1,7 @@
+// public/js/config.js
+// ⚠️ NUNCA COMMITAR ESTE ARQUIVO NO GITHUB!
+
+window.ENV = {
+  SUPABASE_URL: 'https://cqvkfrojkjicfxipwltz.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxdmtmcm9qa2ppY2Z4aXB3bHR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzOTQzNzksImV4cCI6MjA4Nzk3MDM3OX0.THdrIPT1L9l3WPD3ltuI4oR0ggAn-MUi_FCqPfBobDE'
+};
