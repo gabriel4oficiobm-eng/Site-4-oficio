c30abedf7479ce9cbf1573906bafb6e806a5572b
gabriel4oficiobm-eng <gabriel4oficiobm@gmail.com>
2026-03-04 22:57:06 -0300

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
     
-    const SUPABASE_URL = 'https://cqvkfrojkicfxipwltz.supabase.co';
+    const SUPABASE_URL = 'https://cqvkfrojkjicfxipwltz.supabase.co';
     const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxdmtmcm9qa2ppY2Z4aXB3bHR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzOTQzNzksImV4cCI6MjA4Nzk3MDM3OX0.THdrIPT1L9l3WPD3ltuI4oR0ggAn-MUi_FCqPfBobDE';
     
     if (typeof supabase === 'undefined') {
